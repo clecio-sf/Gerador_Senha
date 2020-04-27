8a4b6a8dda7754ec11ea4a74da29aa72cc3d0c66
Clecio Silva <clecio-sf@hotmail.com>
2020-04-27 00:51:59 -0300

    novo commit

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+const pass = document.querySelector('.geraSenha')
+const tamanho = document.querySelector('.tamanho')
+let letras = 'abcdefghijklmnopqrtuwxyz'
+
+
+// tamanho.addEventListener('keypress', function (e) {
+//   if (e.keycode === 13) {
+//     if (!tamanho.value) return
+//     makePass(tamanho.value)
+//   }
+// })
+
+// pega todos os clicks
+document.addEventListener('keypress', function (e) {
+  const elemento = e.target
+  if (e.which == 13) {
+    if (elemento.classList.contains('tamanho')) {
+      function makePass(num) {
+        var result = '';
+        for (var i = 0; i < num; i++) {
+          result += letras.charAt(Math.floor(Math.random() * letras.length));
+        }
+        return result;
+      }
+      pass.innerHTML = makePass(tamanho.value)
+    }
+  }
+  if (elemento.classList.contains('minuscula')) {
+    console.log('clicou')
+  }
+
+  if (elemento.classList.contains('numero')) {
+    console.log('clicou')
+  }
+
+  if (elemento.classList.contains('simbolo')) {
+    console.log('clicou')
+  }
+})
+
