9745d6696ca4e9c34f14d98a50fc255a0317339f
Clecio Silva <clecio-sf@hotmail.com>
2020-04-27 09:09:37 -0300

    novo commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,39 @@
 const pass = document.querySelector('.geraSenha')
 const tamanho = document.querySelector('.tamanho')
-let letras = 'abcdefghijklmnopqrtuwxyz'
 
-
-// pega todos os clicks
-document.addEventListener('keypress', function (e) {
-  const elemento = e.target
+tamanho.addEventListener('keypress', function (e) {
   if (e.which == 13) {
-    if (elemento.classList.contains('tamanho')) {
-      function makePass(num) {
-        var result = '';
-        for (var i = 0; i < num; i++) {
-          result += letras.charAt(Math.floor(Math.random() * letras.length));
-        }
-        return result;
-      }
-      pass.innerHTML = makePass(tamanho.value)
-    }
-  }
-  if (elemento.classList.contains('minuscula')) {
-    console.log('clicou')
-  }
-
-  if (elemento.classList.contains('numero')) {
-    console.log('clicou')
-  }
-
-  if (elemento.classList.contains('simbolo')) {
-    console.log('clicou')
+    if (!tamanho.value) return
+    pass.innerHTML = makePass(tamanho.value)
   }
 })
 
+// pega todos os clickes no checkbox
+document.addEventListener('click', function (e) {
+  const elemento = e.target
+  if (elemento.classList.contains('minuscula')) {
+    console.log('clicou ')
+  }
+})
+
+function makePass(num) {
+  let result = ''
+  for (var i = 0; i < num; i++) {
+    result += getRandomChar()
+  }
+  return result
+}
+console.log(makePass(10))
+
+function getRandomChar() {
+  /*
+ *    array contendo indices  da tabela ascii para retornar caracteres selecionados
+ *    [48, 57] = numeros
+ *    [64, 90] = @ e letras maiusculas
+ *    [97, 122] = letras minusculas
+ */
+  let asciiMinusculas = [[97, 122]]
+  let i = Math.floor(Math.random() * asciiMinusculas.length);
+  return String.fromCharCode(Math.floor(Math.random() * (asciiMinusculas[i][1]
+    - asciiMinusculas[i][0])) + asciiMinusculas[i][0])
+}
