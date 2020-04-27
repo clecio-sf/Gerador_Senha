355b630d0c5760958c6dc5017e662af482af26ba
Clecio Silva <clecio-sf@hotmail.com>
2020-04-27 00:59:44 -0300

    novo commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,17 +11,30 @@ let letras = 'abcdefghijklmnopqrtuwxyz'
 // })
 
 // pega todos os clicks
-tamanho.addEventListener('keypress', function (e) {
+document.addEventListener('keypress', function (e) {
+  const elemento = e.target
   if (e.which == 13) {
-    console.log(tamanho.value)
+    if (elemento.classList.contains('tamanho')) {
+      function makePass(num) {
+        var result = '';
+        for (var i = 0; i < num; i++) {
+          result += letras.charAt(Math.floor(Math.random() * letras.length));
+        }
+        return result;
+      }
+      pass.innerHTML = makePass(tamanho.value)
+    }
+  }
+  if (elemento.classList.contains('minuscula')) {
+    console.log('clicou')
+  }
+
+  if (elemento.classList.contains('numero')) {
+    console.log('clicou')
+  }
+
+  if (elemento.classList.contains('simbolo')) {
+    console.log('clicou')
   }
 })
 
-
-function makePass(num) {
-  var result = '';
-  for (var i = 0; i < num; i++) {
-    result += letras.charAt(Math.floor(Math.random() * letras.length));
-  }
-  return result; pass.innerHTML = makePass(tamanho.value)
-}
